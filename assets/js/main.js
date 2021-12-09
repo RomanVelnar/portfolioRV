e50df00636df64974ac1c5f60219106d1bd8879a
Roman Velnar <roman.velnar@gmail.com>
2021-12-09 20:48:13 +0000

    Added JS for navbar and mostly done with the structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,53 @@
 // button 
-mybutton = document.getElementById("buttonTop");
+// mybutton = document.getElementById("buttonTop");
 
-window.onscroll = function() {getButton()};
+// window.onscroll = function() {getButton()};
 
-function getButton() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
+// function getButton() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         mybutton.style.display = "block";
+//     } else {
+//         mybutton.style.display = "none";
+//     }
+// };
 
-function buttonFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+// function buttonFunction() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// };
 
 // Open box 
-function openBox(e, boxId){
-    var i, boxContent, box;
+// function openBox(e, boxId){
+//     var i, boxContent, box;
     
-    boxContent = document.getElementsByClassName('boxContent');
-    for(i=0; i<boxContent.length; i++){
-        boxContent[i].style.display = "none";
-    }
-    box = document.getElementsByClassName('box');
-    for(i=0; i<box.length; i++){
-        box[i].className = box[i].className.replace('active', "")
-    }
+//     boxContent = document.getElementsByClassName('boxContent');
+//     for(i=0; i<boxContent.length; i++){
+//         boxContent[i].style.display = "none";
+//     }
+//     box = document.getElementsByClassName('box');
+//     for(i=0; i<box.length; i++){
+//         box[i].className = box[i].className.replace('active', "")
+//     }
 
-    document.getElementById(boxId).style.display = "block";
-    e.currentTarget.className += 'active';
-}
+//     document.getElementById(boxId).style.display = "block";
+//     e.currentTarget.className += 'active';
+// };
 
+// Navigation
+
+const navToggle1 = document.querySelector('.navToggle');
+const navLinks1 = document.querySelectorAll('.navbarLink')
+
+
+navToggle1.addEventListener('click', () => {
+    document.body.classList.toggle('navOpen');
+});
+
+navLinks1.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('navOpen');
+    })
+});
 
 
 
