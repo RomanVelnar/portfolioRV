5b2856c783ad3e63ffce20914f790001df572065
Roman Velnar <roman.velnar@gmail.com>
2021-12-10 21:14:16 +0000

    Changed the navbar and added some animations JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,46 @@
 // button 
-// mybutton = document.getElementById("buttonTop");
+mybutton = document.getElementById("buttonTop");
 
-// window.onscroll = function() {getButton()};
+window.onscroll = function() {getButton()};
 
-// function getButton() {
-//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//         mybutton.style.display = "block";
-//     } else {
-//         mybutton.style.display = "none";
-//     }
-// };
+function getButton() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+};
 
-// function buttonFunction() {
-//     document.body.scrollTop = 0;
-//     document.documentElement.scrollTop = 0;
-// };
+function buttonFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
+
+
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navbarList');
+    const navLinks = document.querySelectorAll('.navbarList li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = 'navbarListFade 0.5s ease forwards ${index / 7 + 1.5}s'
+            }  
+        });
+        burger.classList.toggle('toggle');
+
+    });
+
+
+}
+
+navSlide();
 
 // Open box 
 // function openBox(e, boxId){
@@ -35,19 +61,51 @@
 
 // Navigation
 
-const navToggle1 = document.querySelector('.navToggle');
-const navLinks1 = document.querySelectorAll('.navbarLink')
+// const navToggle1 = document.querySelector('.navToggle');
+// const navLinks1 = document.querySelectorAll('.navbarLink');
 
 
-navToggle1.addEventListener('click', () => {
-    document.body.classList.toggle('navOpen');
-});
+// navToggle1.addEventListener('click', () => {
+//     document.body.classList.toggle('navOpen');
+// });
 
-navLinks1.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('navOpen');
-    })
-});
+// navLinks1.forEach(link => {
+//     link.addEventListener('click', () => {
+//         document.body.classList.remove('navOpen');
+//     })
+// });
+
+
+// Text animation
+
+const text = document.querySelector(".sectionTitle");
+const sText = text.textContent;
+const splText = sText.split("");
+text.textContent = "";
+
+for(let i=0; i < splText.length; i++) {
+    text.innerHTML += "<span>" + splText[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(tick, 50);
+
+function tick() {
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++
+    if(char === splText.length){
+        complete();
+        return;
+    }
+}
+
+function complete(){
+    clearInterval(timer);
+    timer = null;
+}
+
+
 
 
 
